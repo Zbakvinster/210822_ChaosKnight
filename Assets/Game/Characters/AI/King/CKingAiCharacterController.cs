175517876b7baa5a7104dadd75adff2974a8e863
TomasKazikAG <tomas.kazik@w.aldagames.com>
2021-08-23 22:17:28 +0200

    King prioritizuje Playera, pokud je dost blizko - jinak utoci na nejblizsi target

--- Assets/Game/Characters/AI/King/CKingAiCharacterController.cs
+++ Assets/Game/Characters/AI/King/CKingAiCharacterController.cs
@@ -6,13 +6,25 @@ namespace Game.Characters.AI.King
     public class CKingAiCharacterController : CFightingAiCharacterController
     {
         [SerializeField] private CPlayerCharacterController _player;
+        [SerializeField] private float _playerAggroDistance;
+        private float _sqrPlayerAggroDistance;
 
         protected override void Start()
         {
             base.Start();
 
-            _target = _player;
+            _sqrPlayerAggroDistance = _playerAggroDistance * _playerAggroDistance;
+            
             CGameManager.Instance.AddCitySide(this);
+        }
+
+        protected override void Update()
+        {
+            _target = (_player.transform.position - _cachedTransform.position).sqrMagnitude <= _sqrPlayerAggroDistance
+                ? _player
+                : CGameManager.Instance.GetClosesChaosUnit(_cachedTransform.position);
+            
+            base.Update();
         }
     }
 }
