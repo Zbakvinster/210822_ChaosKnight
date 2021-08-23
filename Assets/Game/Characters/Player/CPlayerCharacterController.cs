06f1e404e346dc6ab7733a763a79e9fb452cadb5
TomasKazikAG <tomas.kazik@w.aldagames.com>
2021-08-23 23:32:49 +0200

    Basic cinemachine

--- Assets/Game/Characters/Player/CPlayerCharacterController.cs
+++ Assets/Game/Characters/Player/CPlayerCharacterController.cs
@@ -5,38 +5,40 @@ namespace Game.Characters.Player
     public class CPlayerCharacterController : CBaseCharacter
     {
         [SerializeField] private CharacterController _characterController;
+        [SerializeField] private Transform _cachedGraphicsTransform;
+        [SerializeField] private Transform _cameraFollowTarget;
         [SerializeField] private float _movementSpeed;
         [SerializeField] private float _rotationSpeed;
 
         private const float GRAVITY = 9.8f;
         private float _fallSpeed;
-        private Transform _cachedTransform;
 
         protected override void Start()
         {
             base.Start();
 
-            _cachedTransform = transform;
             CGameManager.Instance.AddChaosSide(this);
         }
 
         public void Update()
         {
             float deltaTime = Time.deltaTime;
+            _cameraFollowTarget.Rotate(Vector3.up, Input.GetAxis("Mouse X") * _rotationSpeed * deltaTime);
 
-            _cachedTransform.Rotate(Vector3.up, Input.GetAxis("Mouse X") * _rotationSpeed * deltaTime);
-            
             if (_characterController.isGrounded)
                 _fallSpeed = 0;
 
             _fallSpeed -= GRAVITY * deltaTime;
 
-            Vector3 direction = Vector3.ClampMagnitude(
-                    Input.GetAxis("Vertical") * _cachedTransform.forward
-                        + Input.GetAxis("Horizontal") * _cachedTransform.right,
-                    1)
-                + new Vector3(0, _fallSpeed, 0);
+            Vector3 moveDirection = Vector3.ClampMagnitude(
+                Input.GetAxis("Vertical") * _cameraFollowTarget.forward
+                    + Input.GetAxis("Horizontal") * _cameraFollowTarget.right,
+                1);
+            Vector3 direction = moveDirection + new Vector3(0, _fallSpeed, 0);
 
+            if (moveDirection != Vector3.zero)
+                _cachedGraphicsTransform.forward = moveDirection;
+            
             _characterController.Move(direction * (_movementSpeed * deltaTime));
         }
     }
