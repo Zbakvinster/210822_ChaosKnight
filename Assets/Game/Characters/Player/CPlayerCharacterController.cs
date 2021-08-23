206a36e8f4d4f0ee292e4656def0156d38997647
TomasKazikAG <tomas.kazik@w.aldagames.com>
2021-08-23 23:03:33 +0200

    Player rotation

--- Assets/Game/Characters/Player/CPlayerCharacterController.cs
+++ Assets/Game/Characters/Player/CPlayerCharacterController.cs
@@ -6,27 +6,34 @@ namespace Game.Characters.Player
     {
         [SerializeField] private CharacterController _characterController;
         [SerializeField] private float _movementSpeed;
+        [SerializeField] private float _rotationSpeed;
 
         private const float GRAVITY = 9.8f;
         private float _fallSpeed;
+        private Transform _cachedTransform;
 
         protected override void Start()
         {
             base.Start();
 
+            _cachedTransform = transform;
             CGameManager.Instance.AddChaosSide(this);
         }
 
         public void Update()
         {
+            float deltaTime = Time.deltaTime;
+
+            _cachedTransform.Rotate(Vector3.up, Input.GetAxis("Mouse X") * _rotationSpeed * deltaTime);
+            
             if (_characterController.isGrounded)
                 _fallSpeed = 0;
 
-            float deltaTime = Time.deltaTime;
             _fallSpeed -= GRAVITY * deltaTime;
-            
+
             Vector3 direction = Vector3.ClampMagnitude(
-                    Input.GetAxis("Vertical") * Vector3.forward + Input.GetAxis("Horizontal") * Vector3.right,
+                    Input.GetAxis("Vertical") * _cachedTransform.forward
+                        + Input.GetAxis("Horizontal") * _cachedTransform.right,
                     1)
                 + new Vector3(0, _fallSpeed, 0);
 
