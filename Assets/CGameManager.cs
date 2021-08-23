b648341372e7488cd298069b5a475e6b75f6be0a
TomasKazikAG <tomas.kazik@w.aldagames.com>
2021-08-23 20:27:38 +0200

    King followujici hrace

--- Assets/CGameManager.cs
+++ Assets/CGameManager.cs
@@ -1,12 +1,23 @@
+using System;
+using Game.Characters.AI;
 using Game.Characters.Player;
 using UnityEngine;
 
 public class CGameManager : MonoBehaviour
 {
     [SerializeField] private CPlayerCharacterController _playerCharacterController;
+    [SerializeField] private CKingAiCharacterController _kingCharacterController;
+
+    private void Start()
+    {
+        _kingCharacterController.Init(_playerCharacterController);
+    }
 
     private void Update()
     {
-        _playerCharacterController.OnUpdate(Time.deltaTime);
+        float deltaTime = Time.deltaTime;
+        
+        _playerCharacterController.OnUpdate(deltaTime);
+        _kingCharacterController.OnUpdate(deltaTime);
     }
 }
---
+++ Assets/Game/Characters/AI/CKingAiCharacterController.cs
@@ -0,0 +1,22 @@
+﻿using Game.Characters.Player;
+using UnityEngine;
+
+namespace Game.Characters.AI
+{
+    public class CKingAiCharacterController : CBaseAiCharacter
+    {
+        private CPlayerCharacterController _player;
+        private Transform _playerTransformCached;
+
+        public void Init(CPlayerCharacterController player)
+        {
+            _player = player;
+            _playerTransformCached = _player.transform;
+        }
+        
+        public override void OnUpdate(float deltaTime)
+        {
+            _navMeshAgent.SetDestination(_playerTransformCached.position);
+        }
+    }
+}
