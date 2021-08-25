1f56cf8388b6d14b64654859442432f84202d871
TomasKazikAG <tomas.kazik@w.aldagames.com>
2021-08-25 21:07:11 +0200

    Ingame menu - reset hry

---
+++ Assets/Game/UI/CIngameMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Game.UI
+{
+    public class CIngameMenu : MonoBehaviour
+    {
+        public void RestartGame()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
